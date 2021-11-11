23d97390bef8c00cfc60392cc74e084f947208c4
Bilal Tariq <skillzaa.com@gmail.com>
2021-11-11 08:26:08 +0500

    have to upload this feature today

--- src/tasks/create_dir_structure.rs
+++ src/tasks/create_dir_structure.rs
@@ -1,31 +1,43 @@
-use std::io::{Error};
+use std::io::{Error,ErrorKind};
 use crate::bro; //internallythe core of brown is known as bro
+use qndr;
 
-pub fn run()->Result<bool,Error>{
-    let paths_list = vec!
-            [ 
-                "data", 
-                "site" ,
-                "site/images", 
-                "hulkfolder" ,
-                "hulkfolder/templates" ,
-                "hulkfolder/config" ,
-            ];
-let parent_folder = "./cds/";
+pub fn run(parent_folder:&str,paths_list:Vec<&str>)
+->Result<bool,Error>{
 let mut nv:Vec<String> = Vec::new();
-
     for path in paths_list {
+    //----sanitize
+        if !sanitize(path) {
+            println!("path not correct: {}",path);
+            continue;
+        }
+    
         let mut a = String::from(parent_folder);
         a.push_str(path);
         nv.push(a);
     }
-            bro::create_dir("./cds")?;
-            create_dir_structure(&nv)?;
+    let parent_path = format!("./{}/", &parent_folder);
+    bro::create_dir(parent_path.as_str())?;
+    create_structure(&nv)?;
 Ok(true)                    
 }
-
-fn create_dir_structure(path_list:&Vec<String>)->Result<bool,Error>{
-
+fn sanitize(input:&str)->bool{
+let answer_one =  qndr::begin_with_alphabet(input);
+let answer_two  = qndr::alphabets_with_symbols(input, "\\/");
+if answer_one && answer_two {true}else {false}
+}
+fn bool_to_result(input:bool,message:&str)->Result<bool,Error>{
+    match input {
+        true=>{
+            return Ok(true);
+        },
+        false=>{
+            let e = Error::new(ErrorKind::InvalidData,message);
+            return Err(e);
+        },
+    }
+}
+fn create_structure(path_list:&Vec<String>)->Result<bool,Error>{
 
 for folder in path_list {
     let result = bro::create_dir(folder);
--- tests/create_dir_structure_test.rs
+++ tests/create_dir_structure_test.rs
@@ -4,5 +4,23 @@ use brown;
 #[cfg(test)]
 #[test]
 fn use_fn(){
-    let a = brown::create_dir_structure::run();
+    let paths_list = vec!
+            [ 
+                "/hulkfolder" ,
+                "./data2", 
+                "si?te" ,
+                "data", 
+                "site/images", 
+                "hulkfolder/templates" ,
+            ];
+let parent_folder = "cds";
+    let a = brown::create_dir_structure::
+    run(parent_folder,paths_list);
+    //====== tests
+    assert_eq!(true,brown::path_exists("cds/data"));
+    assert_eq!(true,brown::path_exists("cds/site/images"));
+    assert_eq!(true,brown::path_exists("cds/hulkfolder/templates"));
+//===clean up
+    let destroy = brown::remove_dir_brute(parent_folder).unwrap();
+    assert_eq!(destroy,true);
 }
