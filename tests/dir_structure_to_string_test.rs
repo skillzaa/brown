eb5694d5631ea4879689d91eb8d2112e6c57c195
skilzaa <skillzaa.com@gmail.com>
2021-11-26 16:36:32 +0500

    finally made the loop work ...to over write a vec do not use let x or a new x will be created just x=y;

--- tests/dir_structure_to_string_test.rs
+++ tests/dir_structure_to_string_test.rs
@@ -1,40 +1,60 @@
 use brown as bro;
+use std::fs::DirEntry;
+use std::io::{Error,ErrorKind};
 use brown::tasks;
 mod setup;
 use setup::*;
 #[cfg(test)]
 #[test]
-fn use_fn(){
+fn loop_test(){
 let _ = setup::build_up_dirs();
 let mut output:Vec<String> = Vec::new();
-//--level 0 here--the parent folder name
-// --check if thr source folder exist
-//-- if yes get it in there--later
-//--now get level one-- this level takes string while all the itterations after this takes DirEntry thus this one is kept seperate for simpleicity.
 let level_one_de = 
 bro::get_dirs(setup::PARENTFOLDER).unwrap();
-
 let mut level_one_str = 
 bro::direntry_to_path_all(&level_one_de, true)
 .unwrap();
 // get level one string into output
 output.append(&mut level_one_str);
+//=============================  
+// This is from level 2 onwars (lvl 0 and 1 done)  
+//=============================    
+let mut seed = level_one_de;
+loop {
+let sub_dirs = 
+bro::get_dirs_all(&seed).unwrap();   
+if sub_dirs.len() < 1 {break;}
 
-//==========level -2
-let level_two_de = 
-bro::get_dirs_all(&level_one_de).unwrap();
-let mut level_two_str = 
-bro::direntry_to_path_all(&level_two_de, true).unwrap();
-output.append(&mut level_two_str);
+let mut paths = 
+bro::direntry_to_path_all(&sub_dirs, true).unwrap();
 
-//==========level -3
-let level_three_de = 
-bro::get_dirs_all(&level_two_de).unwrap();
-let mut level_three_str = 
-bro::direntry_to_path_all(&level_three_de, true).unwrap();
-output.append(&mut level_three_str);
+output.append(&mut paths);
+// for s in sub_dirs {
+    
+// }
+// let mut seed:Vec<DirEntry> = Vec::new();
+seed = sub_dirs;
+}
+//=============================    
 
-
-//  let _ = setup::tear_down();
+let _ = setup::tear_down();
+check_final(&output);
 println!("{:#?}",output);
+}
+
+fn check_final(output:&Vec<String>)->bool {
+let v: Vec<&str> = output.iter().map(|s| &**s).collect();
+    assert_eq!(v,[
+        "./tests/delme/b",
+        "./tests/delme/a",
+        "./tests/delme/c",
+        "././tests/delme/a/a2",
+        "././tests/delme/a/a1",
+        "././tests/delme/a/a3",
+        "././tests/delme/c/c1",
+        "././tests/delme/c/c2",
+        "./././tests/delme/c/c2/c22",
+        "./././tests/delme/c/c2/c21",
+    ]);
+    true
 }
