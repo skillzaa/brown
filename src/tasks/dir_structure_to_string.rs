b4f60a08c33d2b3461edaff22f95299ca5bb9c1c
skilzaa <skillzaa.com@gmail.com>
2021-11-26 17:39:06 +0500

    finally Alhamdullah created a string structure of a folder, In this alone I created walkdir

--- src/tasks/dir_structure_to_string.rs
+++ src/tasks/dir_structure_to_string.rs
@@ -2,55 +2,39 @@
 use std::io::{Error,ErrorKind};
 use std::fs::DirEntry;
 use crate::bro;
-
 pub fn run(source_folder:&str)->Result<Vec<String>,Error>{
-// // check the source folder exists, if not Error
-// let _ =check_source_folder(source_folder)?;
-
-// // place the source folder path in output
 let mut output:Vec<String> = Vec::new();
-// output.push(String::from(source_folder));
-
-// // The Loop
-//     // get into the loop (break on finished)
-//     // - get dirs all
-//     // - direntry to path_all
-//     // - inser into out put
-// // prep for loop    
-// let mut finished = false;
-
-// let mut level_de:Vec<DirEntry> = 
-// bro::get_dirs(source_folder)?;
-
-// let mut level_str = 
-// bro::direntry_to_path_all(&level_de, true)?;
-
-// let mut counter = 0;
-// //=====LOOP====
-// loop {
-
-// if level_de.len() < 1 {finished = true;}
-// if counter > 100 {finished = true;}
-// if finished{break;}
-// //--save new level_str
-// let temp = bro::get_dirs_all(&level_de)?;
-
-// //---get the new level_str into output
-//  for n in level_str {
-//     //  let p = bro::direntry_to_path(&n)?;
-//      output.push(n);
-//  }
-//  //imp
-// let level_str = temp;
-//  counter = counter + 1;
-// }
-Ok(output)                    
+//===level zero =======
+if !bro::path_exists(source_folder) {
+    return Err(Error::new(ErrorKind::NotFound,"the source folder does not exist"));
 }
-// fn check_source_folder(source_folder:&str)->Result<bool,Error>{
-//     if !bro::path_exists(&source_folder) {
-//         let e = Error::new(ErrorKind::NotFound,"the source folder could not be found");
-//         return Err(e);
-//     }else {
-//         Ok(true)
-//     }
-//}
+output.push(String::from(source_folder));
+//======lvl 0 ends=========
+
+//======lvl 1 =========
+let level_one_de = 
+bro::get_dirs(source_folder)?;
+
+let mut level_one_str = 
+bro::direntry_to_path_all(&level_one_de, true)
+?;
+output.append(&mut level_one_str);
+//=============================  
+// This is from level 2 onwards (lvl 0 and 1 done)  
+//=============================    
+    let mut seed = level_one_de;
+loop {
+let sub_dirs = 
+bro::get_dirs_all(&seed)?;   
+if sub_dirs.len() < 1 {break;}
+
+let mut paths = 
+bro::direntry_to_path_all(&sub_dirs, true)
+?;
+
+output.append(&mut paths);
+seed = sub_dirs;
+}
+   Ok(output) 
+}
+    
---
+++ tests/dir_structure_to_string_real.rs
@@ -0,0 +1,36 @@
+use brown as bro;
+use std::fs::DirEntry;
+use std::io::{Error,ErrorKind};
+use brown::tasks;
+mod setup;
+use setup::*;
+#[cfg(test)]
+#[test]
+fn loop_test(){
+let _ = setup::build_up_dirs();
+let output = bro::tasks::dir_structure_to_string
+::run(setup::PARENTFOLDER).unwrap();
+
+let _ = setup::tear_down();
+// println!("{:#?}",output);
+check_final(&output);
+}
+
+fn check_final(output:&Vec<String>)->bool {
+let v: Vec<&str> = output.iter().map(|s| &**s).collect();
+  
+assert_eq!(v,[
+        "tests/delme/",
+        "./tests/delme/b",
+        "./tests/delme/a",
+        "./tests/delme/c",
+        "././tests/delme/a/a2",
+        "././tests/delme/a/a1",
+        "././tests/delme/a/a3",
+        "././tests/delme/c/c1",
+        "././tests/delme/c/c2",
+        "./././tests/delme/c/c2/c22",
+        "./././tests/delme/c/c2/c21",
+    ]);
+    true
+}
