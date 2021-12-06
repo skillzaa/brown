96b1364e8887c44d6b9671e35ddc41e3f5f4880d
skilzaa <skillzaa.com@gmail.com>
2021-12-06 13:35:19 +0500

    improving just the first function

--- src/bro/create_dir.rs
+++ src/bro/create_dir.rs
@@ -3,26 +3,27 @@ use std::io::{Error,ErrorKind};
 use std::path::Path;
 
 use crate::bro_path::BroPath;
-/// The create_dir function will create a folder safely i.e in case the folder does not exist, it will be create But incase the folder already exists the function will return an error.
-/// # Example (folder does not exist)
+/// The create_dir function will create a folder safely i.e in case the folder does not exist, it will be created But incase the folder already exists the function will return an error.
+/// # Example (If folder does not exist)
 /// ```rust
 /// let p_dir = brown::create_dir("random_folder");
 ///  assert!(p_dir.is_ok());
-/// // clean up
-/// let clean = brown::remove_dir_brute("random_folder");
-/// assert!(clean.is_ok());
-/// ``` 
-/// # Example (folder already exists)
-/// ```rust
-/// let p_dir = brown::create_dir("random_folder");
-///  assert!(p_dir.is_ok());
-/// let p_dir_again = brown::create_dir("parent");
-///  assert!(p_dir_again.is_err());
 /// // clean up
 /// let clean = brown::remove_dir_brute("random_folder");
 /// assert!(clean.is_ok());
 /// ``` 
 /// Incase you want to create a fersh folder every time you run your code, you can delete a folder using **remove_dir_brute** and then run **create_dir** with out the fear of an error.
+/// # Example (if folder already exists)
+/// ```rust
+/// let p_dir = brown::create_dir("random_folder");
+///  assert!(p_dir.is_ok());
+/// let p_dir_again = brown::create_dir("random_folder");
+///  assert!(p_dir_again.is_err());
+/// // clean up
+/// let clean = brown::remove_dir_brute("random_folder");
+/// assert!(clean.is_ok());
+/// ``` 
+
 pub fn create_dir( dir_path:&str)->Result<bool,Error> {
     let bp = BroPath::new();
     bp.sanitize(&dir_path.to_string())?;
