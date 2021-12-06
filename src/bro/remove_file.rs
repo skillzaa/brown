d10957da828605716f17d074bc7bdcc934c111d9
skilzaa <skillzaa.com@gmail.com>
2021-12-06 18:26:48 +0500

    imprvoed plz make 1 fn =  1 commit

--- src/bro/remove_file.rs
+++ src/bro/remove_file.rs
@@ -5,6 +5,16 @@ use std::path::Path;
 /// The remove_file method will delete the file on the
 /// given path.
 /// It is a wrapper around fs::remove_file
+/// 
+/// # Example
+/// ```rust
+///  // Make sure the file exists
+///  let created = brown::create_file_brute("some_file.md");
+/// assert!(created.is_ok());
+/// let deleted = brown::remove_file("some_file.md");
+/// assert!(deleted.is_ok());   
+/// ```
+
 pub fn remove_file(file_path:&str)->Result<bool,Error>{
     let path = Path::new(file_path);
         let result  = fs::remove_file(&path);
@@ -15,3 +25,14 @@ pub fn remove_file(file_path:&str)->Result<bool,Error>{
 }
 
 
+mod tests {
+    use super::*;
+    use super::super::*;
+#[test]
+fn basic(){
+ let created = create_file_brute("some_file.md");
+ assert!(created.is_ok());
+ let deleted = remove_file("some_file.md");
+ assert!(deleted.is_ok());   
+}    
+}
