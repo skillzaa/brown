f6be77a776d2504a35e20b99554685ba9980f3da
skilzaa <skillzaa.com@gmail.com>
2021-12-08 11:32:58 +0500

    green

--- src/bro/create_dir_structure.rs
+++ src/bro/create_dir_structure.rs
@@ -1,5 +1,5 @@
 use crate::BrownError;
-/// The create_dir_structure will take a &Vec of String (paths) ususally provided by brown::dir_structure_to_string and create a folder structure using that.
+/// The create_dir_structure will take a &Vec of String (paths) ususally provided by dir_structure_to_string and create a folder structure using that.
 pub fn create_dir_structure(paths_list:&Vec<&str>)
 ->Result<bool,BrownError>{
 let mut nv:Vec<String> = Vec::new();
@@ -36,5 +36,44 @@ for folder in path_list {
     }
 }
 Ok(true)
+}
+
+mod tests {
+use super::*;
+use super::super::*;
+#[test]
+fn use_fn(){
+    let paths_list = vec!
+            [ 
+                "/hulkfolder" ,
+                "./data2", 
+                "si?te" ,
+                "data", 
+                "data_with", 
+                "site/images", 
+                "hulkfolder/templates" ,
+            ];
+let a = create_dir_structure
+(&paths_list);
+assert!(a.is_ok());
+    //====== tests
+    assert_eq!(true,path_exists("./data"));
+    assert_eq!(true,path_exists("./data_with"));
+    assert_eq!(true,path_exists("./site/images"));
+    assert_eq!(true,path_exists("./hulkfolder/templates")); 
+// cleanup    
+    // i can also just remove the base folders
+    let data_removed = 
+    remove_dir_brute("data");
+        assert!(data_removed.is_ok());
+    let _ = remove_dir_brute("data_with");
+    let _ = remove_dir_brute("site/images");
+    let _ = remove_dir_brute("./hulkfolder/templates"); 
+    let _ = remove_dir_brute("./hulkfolder"); 
+    let site_removed = 
+    remove_dir_brute("site");
+    assert!(site_removed.is_ok()); 
+    }
+
 
 }
