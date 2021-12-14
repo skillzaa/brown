4dfc0b065694d535cb77c06e1d291238d60c77ae
skilzaa <skillzaa.com@gmail.com>
2021-12-14 07:35:38 +0500

    just writing tests remains

--- src/bro/create_dir_brute.rs
+++ src/bro/create_dir_brute.rs
@@ -3,12 +3,11 @@ use std::path::Path;
 use crate::BrownError;
 use crate::util;
 
-/// The create_dir_brute fn is for times when you  want to a directory to exist. If the folder already exist it will return true, if the folder does not exist it will create it and then return true. Error is returned only when the operating system call for creating folder fails.
+/// The create_dir_brute fn is for times when you  want to create a directory without worring about if that directory already existed or not. If the folder already exists it will return true, if the folder does NOT exist it will create it and then return true. Error is returned only when the operating system call for creating folder fails.
 /// This function is more concerned about creating the folder and less about saving the contents of the folder. This makes it a better candidate for creating setup folders etc.
 /// If a path with non existing top level folders is given to create_dir_brute; those missing folders will also be created.  
 pub fn create_dir_brute( dir_path:&str)->Result<bool,BrownError> {
-   
-    util::sanitize_dir_path(&dir_path.to_string())?;
+   util::sanitize_dir_path(&dir_path.to_string())?;
         let complete = String::from("./") + &dir_path;
         let path = Path::new(&complete);
         //..................................
@@ -28,13 +27,14 @@ pub fn create_dir_brute( dir_path:&str)->Result<bool,BrownError> {
     mod tests {
     use super::*;
     use super::super::*;
+    use crate::BrownError;
     #[test]
     fn basic(){
-        let _ = remove_dir_brute("create_dir_folder");
+        let _ = remove_dir_brute("dir683");
         
-        let parent_created = create_dir_brute("create_dir_folder");
+        let parent_created = create_dir_brute("dir683");
         assert!(parent_created.is_ok());
-        let parent_removed = remove_dir_brute("create_dir_folder");
+        let parent_removed = remove_dir_brute("dir683");
         assert!(parent_removed.is_ok());
     }
     #[test]
@@ -43,11 +43,15 @@ pub fn create_dir_brute( dir_path:&str)->Result<bool,BrownError> {
         let parent_created = 
         create_dir_brute("paren.t");
         assert!(parent_created.is_err());
+        let res = parent_created.unwrap_err();
+        assert_eq!(res,BrownError::NonUrlSafeSymbolFound);
     }
     #[test]
     fn no_dot_slash_allowed(){
         let parent_created = 
         create_dir_brute("./parent");
         assert!(parent_created.is_err());
+        let res = parent_created.unwrap_err();
+        assert_eq!(res,BrownError::PathBeginWithAlphabet);
     }
     }
